c4d665d6bb19c063dd0b1e46a8cd882d83393eb4
LafontRapnouilTristan <bananabegood212@gmail.com>
2024-04-10 14:47:41 +0200

    try

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(
     author="[removed for review]",
     license='GPLv3+',
     install_requires=[
-        'numpy',
-        'pandas',
-        'pyreadr',
-        'scipy',
-        'tensorflow[and-cuda]',
-        'tensorflow-probability[tf]',
-        'ujson',
+        'numpy==1.26.2',
+        'pandas==2.1.3',
+        'pyreadr==0.5.0',
+        'scipy==1.11.4',
+        'tensorflow==2.16.1',
+        'tensorflow-probability[tf]==0.24.0',
+        'ujson==5.8.0'
     ]
 )
